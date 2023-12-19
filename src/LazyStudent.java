d7427bd5cb00effd24a78ce72de4b3e102bf830b
Norayr <asoyannorayr99@gmail.com>
2023-12-19 16:20:40 +0300

    add commit

--- src/LazyStudent.java
+++ src/LazyStudent.java
@@ -1,17 +1,14 @@
-public class LazyStudent {
+public class LazyStudent extends Student {
+    public LazyStudent() {
+    }
+
+    @Override
+    public void study() {
+        System.out.println("Сегодня не учусь, мне лень.");
+    }
+
     public static void main(String[] args) {
         LazyStudent lazyStudent = new LazyStudent();
         lazyStudent.study();
-    }
-    private String name;
-    private String description;
-
-    public LazyStudent() {
-        this.name = "LazyStudent";
-        this.description = "Сегодня не учусь, мне лень.";
-    }
-
-    public void study() {
-        System.out.println("Сегодня не учусь, мне лень.");
     }
 }
